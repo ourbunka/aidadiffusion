bbe9f4ad7fde6cbef03376b6c50f2c8823fef89a
ourbunka <72286679+ourbunka@users.noreply.github.com>
2023-01-09 23:24:27 +0800

    Add files via upload

--- AIDADiffusion/diffusers/cudasd.py
+++ AIDADiffusion/diffusers/cudasd.py
@@ -1,7 +1,7 @@
 import random
 from typing import List, Optional, Union
 
-
+from PIL import Image
 import os
 import sys
 # Add vendor directory to module search path
@@ -11,13 +11,14 @@ vendor_dir = os.path.join(parent_dir, 'Vendor')
 sys.path.append(vendor_dir)
 import torch  #pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117 and then copy to vendor directory
 
+#pip install xformers==0.0.16rc402    install xformers
 
 from diffusers.models import AutoencoderKL, UNet2DConditionModel
 from diffusers.pipeline_utils import DiffusionPipeline
 from diffusers.schedulers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler
+from diffusers import StableDiffusionUpscalePipeline
 
-
-def cudasdfunc(promtInput, iterationsNum, saveName, inputwidth, inputheight ):
+def cudasdfunc(promtInput, iterationsNum, saveName, inputwidth, inputheight ): #add upscalesetting
     #torch.backends.cudnn.benchmark = True
     torch.backends.cuda.matmul.allow_tf32 = True
 
@@ -27,10 +28,25 @@ def cudasdfunc(promtInput, iterationsNum, saveName, inputwidth, inputheight ):
     
     
     schedulersetting = LMSDiscreteScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16)
-    pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+    pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') 
     torch.manual_seed(seed)
 
 
     prompt = promtInput
     image = pipe(prompt, height=inputheight, width=inputwidth, num_inference_steps=iterationsNum, guidance_scale=7.5, eta=0.0).images[0]
-    image.save(saveName)
+    
+    
+    upscaleSetting = 0 #todo replace with upscaleSetting = upscalesetting, need to implement in go api and ui
+
+    if upscaleSetting == 1:
+        image = image.resize((192, 192))
+        upscalePipeline = StableDiffusionUpscalePipeline.from_pretrained("./stable-diffusion-x4-upscaler", torch_dtype=torch.float16).to("cuda") #need at least 24gb of vram for 192px input, 768px upscaled output.
+        upscalePipeline.enable_xformers_memory_efficient_attention
+        image = upscalePipeline(prompt="", image=image).images[0]
+        upscaleSaveName = saveName + "upscaled.png"
+        image.save(upscaleSaveName)
+
+    if upscaleSetting == 0:
+        image.save(saveName)
+    
+   
