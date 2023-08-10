e0dffed980893685d36658a1e57a439e812a2a3f
ourbunka <72286679+ourbunka@users.noreply.github.com>
2023-08-11 06:28:30 +0800

    Add files via upload

--- AIDADiffusion/diffusers/cudaasd_adv.py
+++ AIDADiffusion/diffusers/cudaasd_adv.py
@@ -6,15 +6,11 @@ import os
 import sys
 # Add vendor directory to module search path
 parent_dir = os.path.abspath(os.path.dirname(__file__))
-vendor_dir = os.path.join(parent_dir, 'Vendor')
+vendor_dir = os.path.join(parent_dir, 'vendor')
 
 sys.path.append(vendor_dir)
-import torch  #pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117 and then copy to vendor directory
-
-
-from diffusers.models import AutoencoderKL, UNet2DConditionModel
-from diffusers.pipeline_utils import DiffusionPipeline
-from diffusers.schedulers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler
+from diffusers import DiffusionPipeline, LMSDiscreteScheduler, DDIMScheduler, PNDMScheduler
+import torch
 
 def cudaadvfunc(promtInput, iterationsNum, saveName, userSeedInput, userInputScheduler, userInputGuidance, inputwidth, inputheight ):
     torch.backends.cuda.matmul.allow_tf32 = True
@@ -25,19 +21,19 @@ def cudaadvfunc(promtInput, iterationsNum, saveName, userSeedInput, userInputSch
 
     if userInputScheduler == "lms":
         schedulersetting = LMSDiscreteScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16)
-        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') 
         torch.manual_seed(seed)
     if userInputScheduler == "ddim":
         schedulersetting = DDIMScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16)
-        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') 
         torch.manual_seed(seed)
     if userInputScheduler == "lmsd":
         schedulersetting = LMSDiscreteScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16)
-        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') 
         torch.manual_seed(seed)
     if userInputScheduler == "pndm":
         schedulersetting = PNDMScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16, steps_offset=1, clip_sample=False)
-        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+        pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') 
         torch.manual_seed(seed)
     
     userGuidanceScale = userInputGuidance
--- AIDADiffusion/diffusers/cudasd.py
+++ AIDADiffusion/diffusers/cudasd.py
@@ -4,17 +4,16 @@ from typing import List, Optional, Union
 
 import os
 import sys
-# Add vendor directory to module search path
+
+
 parent_dir = os.path.abspath(os.path.dirname(__file__))
-vendor_dir = os.path.join(parent_dir, 'Vendor')
+vendor_dir = os.path.join(parent_dir, 'vendor')
 
 sys.path.append(vendor_dir)
-import torch  #pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117 and then copy to vendor directory
+from diffusers import DiffusionPipeline
+import torch
 
 
-from diffusers.models import AutoencoderKL, UNet2DConditionModel
-from diffusers.pipeline_utils import DiffusionPipeline
-from diffusers.schedulers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler
 
 
 def cudasdfunc(promtInput, iterationsNum, saveName, inputwidth, inputheight ):
@@ -24,13 +23,14 @@ def cudasdfunc(promtInput, iterationsNum, saveName, inputwidth, inputheight ):
     seed = random.randint(0,9999999999999)
     print(seed)
     print(torch.__version__)
-    
-    
-    schedulersetting = LMSDiscreteScheduler.from_pretrained("./stable-diffusion-2-1", subfolder="scheduler", revision="fp16",torch_dtype=torch.float16)
-    pipe = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", scheduler=schedulersetting).to('cuda') #replaced with stablediffusion-v-1-4 git repo #"CompVis/stable-diffusion-v1-4",use_auth_token=True
+   
     torch.manual_seed(seed)
 
 
     prompt = promtInput
-    image = pipe(prompt, height=inputheight, width=inputwidth, num_inference_steps=iterationsNum, guidance_scale=7.5, eta=0.0).images[0]
+    pipeline = DiffusionPipeline.from_pretrained("./stable-diffusion-2-1", torch_dtype=torch.float16)
+    pipeline.to("cuda")
+    image = pipeline(prompt, height=inputheight, width=inputwidth, num_inference_steps=iterationsNum, guidance_scale=7.5, eta=0.0).images[0]
     image.save(saveName)
+
+
